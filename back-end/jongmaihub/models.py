2693ff517843ce49908cdaa866e2bf42cbc85938
MeHappyLucky <menggy2546@gmail.com>
2023-10-18 22:04:22 +0700

    Implement Appointment Model

--- back-end/jongmaihub/models.py
+++ back-end/jongmaihub/models.py
@@ -39,3 +39,11 @@ class Customer(models.Model):
     name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=10)
     email = models.CharField(max_length=100)
+
+
+class Appointment(models.Model):
+    """Appointment Model represents the action which customers perform to make a service reservation."""
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    date_time = models.DateTimeField()
