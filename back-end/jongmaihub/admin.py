f31ed7b4c8de6993dedc6969e41690931666ffa7
MeHappyLucky <menggy2546@gmail.com>
2023-10-18 22:29:01 +0700

    Register the models in admin.py

--- back-end/jongmaihub/admin.py
+++ back-end/jongmaihub/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(ServiceProvider)
+admin.site.register(Service)
+admin.site.register(Staff)
+admin.site.register(Customer)
+admin.site.register(Appointment)
+admin.site.register(Rating)
