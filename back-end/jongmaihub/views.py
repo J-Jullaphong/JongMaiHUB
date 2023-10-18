97d73312e943ed2a9509af1fc1e3e95b74b50b84
Yasatsawin Kuldejtitipun <yasatsawin.k@ku.th>
2023-10-18 23:26:53 +0700

    Create View pages for ServiceProvider

--- back-end/jongmaihub/views.py
+++ back-end/jongmaihub/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import *
 
-# Create your views here.
+
+class ListServiceProvider(generics.ListCreateAPIView):
+    """ListServiceProvider displays a list of all Service Provider."""
+    queryset = ServiceProvider.objects.all()
+    serializer_class = ServiceProviderSerializer
+
+
+class DetailServiceProvider(generics.RetrieveUpdateDestroyAPIView):
+    """DetailServiceProvider displays details of a Service Provider."""
+    queryset = ServiceProvider.objects.all()
+    serializer_class = ServiceProviderSerializer
