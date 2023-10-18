043ffbe4653825a104f8d29aca888f7243fe9333
MeHappyLucky <menggy2546@gmail.com>
2023-10-18 21:57:00 +0700

    Implement Staff Model

--- back-end/jongmaihub/models.py
+++ back-end/jongmaihub/models.py
@@ -18,3 +18,16 @@ class Service(models.Model):
     type = models.CharField(max_length=100)
     duration = models.DurationField()
     price = models.DecimalField(decimal_places=2, max_digits=10)
+
+
+class Staff(models.Model):
+    """Staff Model represents the staffs give services to customer."""
+    uid = models.CharField(max_length=28, primary_key=True)
+    name = models.CharField(max_length=100)
+    service_provider = models.ForeignKey(ServiceProvider,
+                                         on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    specialty = models.CharField(max_length=100)
+    background = models.CharField(max_length=500)
+    start_work_time = models.TimeField()
+    get_off_work_time = models.TimeField()
