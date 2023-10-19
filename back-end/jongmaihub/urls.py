dc412116de7411aaa3c3d5b2b8bce5b129185e80
Yasatsawin Kuldejtitipun <yasatsawin.k@ku.th>
2023-10-19 13:48:16 +0700

    Fixing urls

--- back-end/jongmaihub/urls.py
+++ back-end/jongmaihub/urls.py
@@ -1,18 +1,19 @@
 from django.urls import path
 from . import views
 
+app_name = 'jongmaihub'
 
 urlpatterns = [
-    path('', views.ListServiceProvider.as_view()),
-    path('<int:pk>/', views.DetailServiceProvider.as_view()),
-    path('', views.ListService.as_view()),
-    path('<int:pk>/', views.DetailService.as_view()),
-    path('', views.ListStaff.as_view()),
-    path('<int:pk>/', views.DetailStaff.as_view()),
-    path('', views.ListCustomer.as_view()),
-    path('<int:pk>/', views.DetailCustomer.as_view()),
-    path('', views.ListAppointment.as_view()),
-    path('<int:pk>/', views.DetailAppointment.as_view()),
-    path('', views.ListRating.as_view()),
-    path('<int:pk>/', views.DetailRating.as_view()),
+    path('service-provider/', views.ListServiceProvider.as_view()),
+    path('service-provider/<str:pk>/', views.DetailServiceProvider.as_view()),
+    path('service/', views.ListService.as_view()),
+    path('service/<int:pk>/', views.DetailService.as_view()),
+    path('staff/', views.ListStaff.as_view()),
+    path('staff/<str:pk>/', views.DetailStaff.as_view()),
+    path('customer/', views.ListCustomer.as_view()),
+    path('customer/<str:pk>/', views.DetailCustomer.as_view()),
+    path('appointment/', views.ListAppointment.as_view()),
+    path('appointment/<int:pk>/', views.DetailAppointment.as_view()),
+    path('rating/', views.ListRating.as_view()),
+    path('rating/<int:pk>/', views.DetailRating.as_view()),
 ]
--- back-end/mysite/settings.py
+++ back-end/mysite/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'mysite.urls.py'
+ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATES = [
     {
