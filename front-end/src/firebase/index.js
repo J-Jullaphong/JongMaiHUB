e4e98bbb66d10da4f5baa9d945dc7eca20ba28fa
Jullaphong JIAMWATTHANALOET <jullaphong03@gmail.com>
2023-10-19 14:44:55 +0700

    Config and connect to Firebase

---
+++ front-end/src/firebase/index.js
@@ -0,0 +1,8 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import firebaseConfig from './config'
+
+
+firebase.initializeApp(firebaseConfig)
+
+export default firebase.auth()
