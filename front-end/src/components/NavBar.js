21749125de367d78d6db58cf69c407fc8467c473
Jullaphong JIAMWATTHANALOET <jullaphong03@gmail.com>
2023-10-20 16:47:35 +0700

    Revise code for the NavBar component

--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -5,6 +5,11 @@ import SearchBar from "./SearchBar";
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
+const categoriesButtonStyle = {
+    textDecoration: "none",
+    color: "#575757",
+};
+
 class NavBar extends Component {
     state = {
         isAuthenticated: false,
@@ -21,48 +26,10 @@ class NavBar extends Component {
     }
 
     render() {
-        if (!this.state.isAuthenticated) {
-            return (
-                <Navbar appearance="subtle">
-                    <Nav>
-                        <Nav.Item href="../../../">
-                            <img
-                                src="images/jongmaihub-logo.png"
-                                alt="JongMaiHUB"
-                                width="150"
-                            />
-                        </Nav.Item>
-                        <Nav.Item icon={<HomeIcon />} href="../../../">
-                            HUB
-                        </Nav.Item>
-                        <Nav.Menu title="Categories">
-                            <Nav.Item>Barber</Nav.Item>
-                            <Nav.Item>Beauty Clinic</Nav.Item>
-                            <Nav.Item>Salon</Nav.Item>
-                            <Nav.Item>Massage</Nav.Item>
-                        </Nav.Menu>
-                    </Nav>
-                    <Nav pullRight>
-                        <Nav.Item>
-                            <SearchBar />
-                        </Nav.Item>
-                        <Nav.Item
-                            href="/login"
-                            style={{
-                                backgroundColor: "#F5D6CC",
-                                color: "#F26030",
-                            }}
-                        >
-                            Login
-                        </Nav.Item>
-                    </Nav>
-                </Navbar>
-            );
-        }
         return (
             <Navbar appearance="subtle">
                 <Nav>
-                    <Nav.Item>
+                    <Nav.Item href="../../../">
                         <img
                             src="images/jongmaihub-logo.png"
                             alt="JongMaiHUB"
@@ -73,26 +40,82 @@ class NavBar extends Component {
                         HUB
                     </Nav.Item>
                     <Nav.Menu title="Categories">
-                        <Nav.Item>Barber</Nav.Item>
-                        <Nav.Item>Beauty Clinic</Nav.Item>
-                        <Nav.Item>Salon</Nav.Item>
-                        <Nav.Item>Massage</Nav.Item>
+                        <Nav.Item>
+                            <a
+                                href="/search?type=barber"
+                                style={categoriesButtonStyle}
+                            >
+                                Barber
+                            </a>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <a
+                                href="/search?type=beauty%20clinic"
+                                style={categoriesButtonStyle}
+                            >
+                                Beauty Clinic
+                            </a>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <a
+                                href="/search?type=salon"
+                                style={categoriesButtonStyle}
+                            >
+                                Salon
+                            </a>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <a
+                                href="/search?type=massage"
+                                style={categoriesButtonStyle}
+                            >
+                                Massage
+                            </a>
+                        </Nav.Item>
                     </Nav.Menu>
                 </Nav>
                 <Nav pullRight>
                     <Nav.Item>
                         <SearchBar />
                     </Nav.Item>
-                    <Nav.Item>{this.state.user.displayName}</Nav.Item>
-                    <Nav.Menu
-                        icon={<Avatar circle src={this.state.user.photoURL} />}
-                    >
-                        <Nav.Item>
-                            <a onClick={() => firebase.auth().signOut()} style={{textDecoration: "none", color: "#F26030"}}>
-                                Sign Out
-                            </a>
+                    {this.state.isAuthenticated ? (
+                        <>
+                            <Nav.Item>{this.state.user.displayName}</Nav.Item>
+                            <Nav.Menu
+                                icon={
+                                    <Avatar
+                                        circle
+                                        src={this.state.user.photoURL}
+                                    />
+                                }
+                            >
+                                <Nav.Item>
+                                    <a
+                                        href="../../../"
+                                        onClick={() =>
+                                            firebase.auth().signOut()
+                                        }
+                                        style={{
+                                            textDecoration: "none",
+                                            color: "#F26030",
+                                        }}
+                                    >
+                                        Sign Out
+                                    </a>
+                                </Nav.Item>
+                            </Nav.Menu>
+                        </>
+                    ) : (
+                        <Nav.Item
+                            href="/login"
+                            style={{
+                                backgroundColor: "#F5D6CC",
+                                color: "#F26030",
+                            }}
+                        >
+                            Login
                         </Nav.Item>
-                    </Nav.Menu>
+                    )}
                 </Nav>
             </Navbar>
         );
