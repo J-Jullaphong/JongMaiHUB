7c78a1a75cb301593c0304cc74fd603e4d9b04a5
Jullaphong JIAMWATTHANALOET <jullaphong03@gmail.com>
2023-10-20 09:47:09 +0700

    Add searchQuery state to SearchBar component

--- front-end/src/components/SearchBar.js
+++ front-end/src/components/SearchBar.js
@@ -3,10 +3,21 @@ import { AutoComplete, InputGroup } from "rsuite";
 import SearchIcon from "@rsuite/icons/Search";
 
 class SearchBar extends Component {
+    state = {
+        searchQuery: "",
+    };
+
+    handleSearchChange = (value) => {
+        this.setState({ searchQuery: value });
+    };
+
     render() {
         return (
             <InputGroup inside style={{ width: "30vw" }}>
-                <AutoComplete />
+                <AutoComplete
+                    onChange={this.handleSearchChange}
+                    value={this.state.searchQuery}
+                />
                 <InputGroup.Addon>
                     <SearchIcon />
                 </InputGroup.Addon>
