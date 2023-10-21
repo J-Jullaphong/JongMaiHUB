ccf1aebcd4020996418a17605cedadc561ceb216
Pichayanon Toochinda <kewkunggt@gmail.com>
2023-10-21 20:13:46 +0700

    Add test NavBar : should display all services when clicks a category

---
+++ front-end/src/test/NavBar.test.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import NavBar from "../components/NavBar";
+
+jest.mock("react-router-dom", () => ({
+    useNavigate: jest.fn(),
+}));
+
+jest.mock("firebase/compat/app", () => ({
+    auth: () => ({
+        onAuthStateChanged: jest.fn(),
+        currentUser: jest.fn(),
+        signOut: jest.fn(),
+    }),
+}));
+
+it('should display hub, categories, search bar and login', () => {
+    render(<NavBar />);
+    const getHub = screen.getByText(/hub/i);
+    const getCategories = screen.getByText(/categories/i);
+    const getSearchBar = screen.getByTitle(/SearchBar/i);
+    const getLogin = screen.getByText(/login/i);
+    expect(getHub).toBeInTheDocument();
+    expect(getCategories).toBeInTheDocument();
+    expect(getSearchBar).toBeInTheDocument();
+    expect(getLogin).toBeInTheDocument();
+});
+
+it('should display all services when clicks a category', () => {
+    render(<NavBar />);
+    fireEvent.click(screen.getByText(/categories/i));
+    // todo use mock api 
+    const getBarber = screen.getByText(/barber/i);
+    expect(getBarber).toBeInTheDocument();
+});
