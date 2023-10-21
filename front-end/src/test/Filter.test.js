9fa8d583762490111b28f44227596d7f71767473
Pichayanon Toochinda <kewkunggt@gmail.com>
2023-10-21 22:48:39 +0700

    Add test Filter : should update query parameters when click apply or clear

--- front-end/src/test/Filter.test.js
+++ front-end/src/test/Filter.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Filter from '../components/Filter';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 
@@ -16,6 +16,7 @@ describe("Filter", () => {
         useNavigate.mockReturnValue(navigateMock);
         useSearchParams.mockReturnValue([searchParamsMock]);
     });
+
     it('should display type, price, duration, apply and clear filter', () => {
         render(<Filter />);
         expect(screen.getByText(/type:/i)).toBeInTheDocument();
@@ -23,5 +24,23 @@ describe("Filter", () => {
         expect(screen.getByText(/duration/i)).toBeInTheDocument();
         expect(screen.getByText(/apply filter/i)).toBeInTheDocument();
         expect(screen.getByText(/clear filter/i)).toBeInTheDocument();
+    });
+
+    it('should display and update query parameters when click apply filter', () => {
+        render(<Filter />);
+        const applyFilterButton = screen.getByText(/apply filter/i);
+        fireEvent.click(applyFilterButton);
+        expect(navigateMock).toHaveBeenCalledWith(expect.stringMatching(/type=&maxPrice=0&maxDuration=0$/));
+    });
+
+    it('should clear all query parameters when click clear filter', () => {
+        searchParamsMock.set('type', 'barber');
+        searchParamsMock.set('maxPrice', '1000');
+        searchParamsMock.set('maxDuration', '60');
+
+        render(<Filter />);
+        const clearFilterButton = screen.getByText(/clear filter/i);
+        fireEvent.click(clearFilterButton);
+        expect(navigateMock).toHaveBeenCalledWith(expect.stringMatching(/type=&maxPrice=0&maxDuration=0$/));
     });
 });
