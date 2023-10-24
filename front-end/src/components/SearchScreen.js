36a327a37da47f8d654b807473097b439245cb27
Jullaphong JIAMWATTHANALOET <jullaphong03@gmail.com>
2023-10-24 23:11:29 +0700

    Implement displaying search results

--- front-end/src/components/SearchScreen.js
+++ front-end/src/components/SearchScreen.js
@@ -1,21 +1,76 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import Filter from "./Filter";
+import DataFetcher from "./DataFetcher";
+import "./styles/SearchScreen.css";
 
 const SearchScreen = () => {
     const [searchQuery] = useSearchParams();
-    const [searchResult] = useState([]);
+    const [serviceData, setServiceData] = useState([]);
+    const [searchResult, setSearchResult] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-    if (!searchResult || searchResult.length === 0) {
+    useEffect(() => {
+        const fetchData = async () => {
+            const dataFetcher = new DataFetcher();
+            try {
+                const serviceData = await dataFetcher.getServiceData();
+                console.log("Received JSON data:", serviceData);
+                setServiceData(serviceData);
+                setLoading(false);
+            } catch (error) {
+                console.error(error);
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        const filterData = () => {
+            const filteredData = serviceData.filter((service) => {
+                return service.name
+                    .toLowerCase()
+                    .includes(searchQuery.get("name").toLowerCase());
+            });
+            setSearchResult(filteredData);
+        };
+        filterData();
+    }, [searchQuery, serviceData]);
+
+    const displayServices = searchResult.map((service) => {
         return (
-            <div style={{ display: "flex" }}>
-                <div style={{ width: "25%" }}>
-                    <Filter />
+            <div className="service" key={service.id}>
+                <img src={service.service_picture} alt={service.name} />
+                <div className="service-detail">
+                <h2>{service.name}</h2>
+                <br />
+                <p>
+                    {service.type} | {service.duration} Minutes |{" "}
+                    {service.price} Baht
+                </p>
                 </div>
-                <h1>SearchQuery = {searchQuery.get("name")}</h1>
             </div>
         );
-    }
+    });
+
+    return (
+        <div style={{ display: "flex" }}>
+            <div className="filter-box">
+                <Filter />
+            </div>
+            <div>
+                {loading ? (
+                    <p>Loading...</p>
+                ) : searchResult.length === 0 ? (
+                    <h1>No Matching Results for {searchQuery.get("name")}</h1>
+                ) : (
+                    displayServices
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default SearchScreen;
