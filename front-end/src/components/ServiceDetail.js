69163588d295889249cd8e17d83a1dff8d85dc32
Jullaphong JIAMWATTHANALOET <jullaphong03@gmail.com>
2023-10-25 22:54:39 +0700

    Add base styling to ServiceDetail

--- front-end/src/components/ServiceDetail.js
+++ front-end/src/components/ServiceDetail.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import "./styles/ServiceDetail.css";
 
 const ServiceDetail = ({ serviceData, providerData }) => {
     const { providerUrl, serviceUrl } = useParams();
@@ -13,17 +14,21 @@ const ServiceDetail = ({ serviceData, providerData }) => {
     );
 
     return (
-        <div>
-            <div className="provider">
-                <img src={provider.profile_picture} alt={provider.name} />
-                <h1>{provider.name}</h1>
+        <div className="detail">
+            <div className="provider-detail">
+                <div className="provider-image">
+                    <img src={provider.profile_picture} alt={provider.name} />
+                </div>
+                <h2>{provider.name}</h2>
                 <p>Open: {provider.opening_time}</p>
                 <p>Close: {provider.closing_time}</p>
                 <p>Location: {provider.location}</p>
             </div>
-            <div className="service">
-                <img src={service.service_picture} alt={service.name} />
-                <h1>{service.name}</h1>
+            <div className="service-detail">
+                <div className="service-image">
+                    <img src={service.service_picture} alt={service.name} />
+                </div>
+                <h2>{service.name}</h2>
                 <p>Duration: {service.duration} Minutes</p>
                 <p>Price: {service.price} Baht</p>
             </div>
