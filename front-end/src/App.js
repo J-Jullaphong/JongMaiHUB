99d100764fae1683ed7a28879e6835530abda6fa
Jullaphong JIAMWATTHANALOET <jullaphong03@gmail.com>
2023-10-25 22:23:07 +0700

    Fix background color bug in webpage

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
 import "firebase/compat/auth";
 
 import HubScreen from "./components/HubScreen";
