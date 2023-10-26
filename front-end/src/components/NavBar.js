052e160861782e07cacf6bb910a6b63228877374
Pichayanon Toochinda <kewkunggt@gmail.com>
2023-10-27 00:05:16 +0700

    Update categories menu make it use data from api

--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -1,7 +1,8 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Avatar, Navbar, Nav } from "rsuite";
 import HomeIcon from "@rsuite/icons/legacy/Home";
 import SearchBar from "./SearchBar";
+import DataFetcher from "./DataFetcher";
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
@@ -10,116 +11,89 @@ const categoriesButtonStyle = {
     color: "#575757",
 };
 
-class NavBar extends Component {
-    state = {
-        isAuthenticated: false,
-        user: "",
-    };
+const NavBar = () => {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [user, setUser] = useState(null);
+    const [serviceData, setServiceData] = useState([]);
 
-    componentDidMount() {
-        firebase.auth().onAuthStateChanged((user) =>
-            this.setState({
-                isAuthenticated: !!user,
-                user: firebase.auth().currentUser,
-            })
-        );
-    }
+    useEffect(() => {
+        const unsubscribe = firebase.auth().onAuthStateChanged((authUser) => {
+            if (authUser) {
+                setIsAuthenticated(true);
+                setUser(authUser);
+            } else {
+                setIsAuthenticated(false);
+                setUser(null);
+            }
+        });
 
-    render() {
-        return (
-            <Navbar appearance="subtle">
-                <Nav>
-                    <Nav.Item href="../../../">
-                        <img
-                            src="images/jongmaihub-logo.png"
-                            alt="JongMaiHUB"
-                            width="150"
-                        />
-                    </Nav.Item>
-                    <Nav.Item icon={<HomeIcon />} href="../../../">
-                        HUB
-                    </Nav.Item>
-                    <Nav.Menu title="Categories">
-                        <Nav.Item>
-                            <a
-                                href="/search?type=barber"
-                                style={categoriesButtonStyle}
-                            >
-                                Barber
-                            </a>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <a
-                                href="/search?type=beauty+clinic"
-                                style={categoriesButtonStyle}
-                            >
-                                Beauty Clinic
-                            </a>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <a
-                                href="/search?type=salon"
-                                style={categoriesButtonStyle}
-                            >
-                                Salon
-                            </a>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <a
-                                href="/search?type=massage"
-                                style={categoriesButtonStyle}
-                            >
-                                Massage
-                            </a>
-                        </Nav.Item>
-                    </Nav.Menu>
-                </Nav>
-                <Nav pullRight>
-                    <Nav.Item title='searchbar'>
-                        <SearchBar />
-                    </Nav.Item>
-                    {this.state.isAuthenticated ? (
-                        <>
-                            <Nav.Item title='username'>{this.state.user.displayName}</Nav.Item>
-                            <Nav.Menu
-                                icon={
-                                    <Avatar title='userprofile'
-                                        circle
-                                        src={this.state.user.photoURL}
-                                    />
-                                }
-                            >
-                                <Nav.Item>
-                                    <a
-                                        href="../../../"
-                                        onClick={() =>
-                                            firebase.auth().signOut()
-                                        }
-                                        style={{
-                                            textDecoration: "none",
-                                            color: "#F26030",
-                                        }}
-                                    >
-                                        Sign Out
-                                    </a>
-                                </Nav.Item>
-                            </Nav.Menu>
-                        </>
-                    ) : (
-                        <Nav.Item
-                            href="/login"
-                            style={{
-                                backgroundColor: "#F5D6CC",
-                                color: "#F26030",
-                            }}
+        const dataFetcher = new DataFetcher();
+        dataFetcher.getServiceData().then((data) => {
+            const uniqueServiceTypes = Array.from(new Set(data.map(service => service.type)));
+            setServiceData(uniqueServiceTypes.map(type => ({ type })));
+        });
+
+        return () => {
+            unsubscribe();
+        };
+    }, []);
+
+    return (
+        <Navbar appearance="subtle">
+            <Nav>
+                <Nav.Item href="../../../">
+                    <img src="images/jongmaihub-logo.png" alt="JongMaiHUB" width="150" />
+                </Nav.Item>
+                <Nav.Item icon={<HomeIcon />} href="../../../">
+                    HUB
+                </Nav.Item>
+                <Nav.Menu title="Categories">
+                    {serviceData.map((service) => (
+                        <a href={`/search?type=${service.type}`} style={categoriesButtonStyle}>
+                            <Nav.Item>{service.type}</Nav.Item>
+                        </a>
+                    ))}
+                </Nav.Menu>
+            </Nav>
+            <Nav pullRight>
+                <Nav.Item title="searchbar">
+                    <SearchBar />
+                </Nav.Item>
+                {isAuthenticated ? (
+                    <>
+                        <Nav.Item title="username">{user.displayName}</Nav.Item>
+                        <Nav.Menu
+                            icon={<Avatar title="userprofile" circle src={user.photoURL} />}
                         >
-                            Login
-                        </Nav.Item>
-                    )}
-                </Nav>
-            </Navbar>
-        );
-    }
-}
+                            <Nav.Item>
+                                <a
+                                    href="../../../"
+                                    onClick={() => firebase.auth().signOut()}
+                                    style={{
+                                        textDecoration: "none",
+                                        color: "#F26030",
+                                    }}
+                                >
+                                    Sign Out
+                                </a>
+                            </Nav.Item>
+                        </Nav.Menu>
+                    </>
+                ) : (
+                    <Nav.Item
+                        href="/login"
+                        style={{
+                            backgroundColor: "#F5D6CC",
+                            color: "#F26030",
+                        }}
+                    >
+                        Login
+                    </Nav.Item>
+                )}
+            </Nav>
+        </Navbar>
+    );
+};
 
 export default NavBar;
+
