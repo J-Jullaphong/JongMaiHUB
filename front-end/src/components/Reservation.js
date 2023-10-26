e2038fc5665e477498ea51bcff6e4d3452bc8441
Yasatsawin Kuldejtitipun <yasatsawin.k@ku.th>
2023-10-27 03:41:01 +0700

    Implement Footer for modal

---
+++ front-end/src/components/Reservation.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import { Modal, DatePicker, Button, Form, Steps } from "rsuite";
+import DataSender from "./DataSender";
+
+const Reservation = ({ service, staff }) => {
+    const [reservationState, setReservationState] = useState(1);
+    const [selectedDateTime, setSelectedDateTime] = useState(new Date());
+    const [isDateSelected, setIsDateSelected] = useState(false);
+
+
+    const handleConfirmClick = () => {
+        return reservationState < 3
+            ? setReservationState(reservationState + 1)
+            : NaN;
+    };
+
+    const handleDateChange = (dateTime) => {
+        setSelectedDateTime(dateTime);
+        setIsDateSelected(!!dateTime);
+    };
+
+    const createFooter = () => {
+        let content;
+
+        if (reservationState === 1) {
+            content = (
+                <>
+                    <Button onClick={handleConfirmClick} appearance="primary">
+                        Continue
+                    </Button>
+                </>
+            );
+        } else if (reservationState === 2) {
+            content = (
+                <>
+                    <Button
+                        onClick={handleConfirmClick}
+                        appearance="primary"
+                        disabled={!isDateSelected}
+                    >
+                        Continue
+                    </Button>
+                </>
+            );
+        } else {
+            content = (
+                <>
+                    <p>Please Confirm your Reservation</p>
+                    <Button onClick={handleConfirmClick} appearance="primary">
+                        Confirm
+                    </Button>
+                </>
+            );
+        }
+
+        return <Modal.Footer>{content}</Modal.Footer>;
+    };
+
+}
+export default Reservation;
