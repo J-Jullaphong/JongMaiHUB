9b3c8c0902c71ea1b87b29c53a78aea9cd4a45a8
Yasatsawin Kuldejtitipun <yasatsawin.k@ku.th>
2023-11-01 16:01:11 +0700

    Add updateData method

--- front-end/src/components/DataSender.js
+++ front-end/src/components/DataSender.js
@@ -24,7 +24,7 @@ class DataSender extends Component {
         for (const [key, value] of formData) {
             if (value instanceof File) {
                 const base64Image = await this.convertImageToBase64(value);
-                updatedFormData.append(key, base64Image); 
+                updatedFormData.append(key, base64Image);
             } else {
                 updatedFormData.append(key, value);
             }
@@ -33,9 +33,20 @@ class DataSender extends Component {
     }
 
 
-    async submitData(type, formData, key = "") {
+    async submitData(type, formData) {
         return await axios
             .post(
+                `${this.state.api}${type}`, formData
+            )
+            .catch((error) => {
+                console.log(error);
+                throw error;
+            });
+    }
+
+    async updateData(type, formData, key = "") {
+        return await axios
+            .patch(
                 `${this.state.api}${type}/${key}`, formData
             )
             .catch((error) => {
@@ -44,28 +55,52 @@ class DataSender extends Component {
             });
     }
 
-    async submitServiceProviderData(formData, key = "") {
-        return this.fetchData("service-provider", formData, key);
+    async submitServiceProviderData(formData) {
+        return this.submitData("service-provider", formData);
     }
 
-    async submitServiceData(formData, key = "") {
-        return this.submitData("service", formData, key);
+    async submitServiceData(formData) {
+        return this.submitData("service", formData);
     }
 
-    async submitStaffData(formData, key = "") {
-        return this.submitData("staff", formData, key);
+    async submitStaffData(formData) {
+        return this.submitData("staff", formData);
     }
 
-    async submitCustomerData(formData, key = "") {
-        return this.submitData("customer", formData, key);
+    async submitCustomerData(formData) {
+        return this.submitData("customer", formData);
     }
 
-    async submitAppointmentData(formData, key = "") {
-        return this.submitData("appointment", formData, key);
+    async submitAppointmentData(formData) {
+        return this.submitData("appointment", formData);
     }
 
-    async submitRatingData(formData, key = "") {
-        return this.submitData("rating", formData, key);
+    async submitRatingData(formData) {
+        return this.submitData("rating", formData);
+    }
+
+    async updateServiceProviderData(formData, key) {
+        return this.updateData("service-provider", formData, key);
+    }
+
+    async updateServiceData(formData, key) {
+        return this.updateData("service", formData, key);
+    }
+
+    async updateStaffData(formData, key) {
+        return this.updateData("staff", formData, key);
+    }
+
+    async updateCustomerData(formData, key) {
+        return this.updateData("customer", formData, key);
+    }
+
+    async updateAppointmentData(formData, key) {
+        return this.updateData("appointment", formData, key);
+    }
+
+    async updateRatingData(formData, key) {
+        return this.updateData("rating", formData, key);
     }
 }
 
