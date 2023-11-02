936d7e9e9771e732b15f4cb741ac572e8f8d41eb
Jullaphong JIAMWATTHANALOET <jullaphong03@gmail.com>
2023-11-02 13:46:56 +0700

    Update NavBar tests

--- front-end/src/test/NavBarAuthen.test.js
+++ front-end/src/test/NavBarAuthen.test.js
@@ -1,39 +1,62 @@
 import React from "react";
 import { render, screen, fireEvent, getByTitle } from "@testing-library/react";
 import NavBar from "../components/NavBar";
+import User from "../components/User";
 
-const currentUserMock = {
-    displayName: "testerName",
-    photoURL: "testerPhoto.jpg",
-};
+const currentUserMock = User.getInstance();
+currentUserMock.setUID("Mock UID");
+currentUserMock.setName("Mock User");
+currentUserMock.setProfilePicture("data:image/jpeg;base64,mockProfilePicture");
 
-jest.mock("firebase/compat/app", () => ({
-    auth: () => ({
-        onAuthStateChanged: (callback) => {
-            callback(currentUserMock);
-        },
-        currentUser: currentUserMock,
-        signOut: jest.fn(),
-    }),
-}));
+const mockServiceData = [
+  {
+    id: 1,
+    name: "Mock Hair Cut",
+    type: "Hair Cut",
+    duration: 30,
+    price: "60.00",
+    service_picture: "data:image/jpeg;base64,mockPicture1",
+    service_provider: "mockProvider1",
+  },
+  {
+    id: 2,
+    name: "Mock Riding",
+    type: "Animal Riding",
+    duration: 30,
+    price: "100.00",
+    service_picture: "data:image/jpeg;base64,mockPicture2",
+    service_provider: "mockProvider1",
+  },
+];
 
 jest.mock("react-router-dom", () => ({
-    useNavigate: jest.fn(),
+  useNavigate: jest.fn(),
 }));
 
 describe("Navbar when user is logged in", () => {
-    it('should display their name and profile', () => {
-        render(<NavBar />);
-        const userName = screen.getByTitle(/username/i);
-        expect(userName).toBeInTheDocument();
-        const userProfile = screen.getByTitle(/userprofile/i);
-        expect(userProfile).toBeInTheDocument();
-    });
+  it("should display their name and profile", () => {
+    render(
+      <NavBar
+        user={currentUserMock}
+        serviceData={mockServiceData}
+        isUserAuthenticated={true}
+      />
+    );
+    const userName = screen.getByTitle(/username/i);
+    expect(userName).toBeInTheDocument();
+    const userProfile = screen.getByTitle(/userprofile/i);
+    expect(userProfile).toBeInTheDocument();
+  });
 
-    it('should display sign out when clicks the profile', () => {
-        render(<NavBar />);
-        fireEvent.click(screen.getByText(/testerName/i));
-        expect(screen.getByText(/sign out/i)).toBeInTheDocument();
-    });
-
+  it("should display sign out when clicks the profile", () => {
+    render(
+      <NavBar
+        user={currentUserMock}
+        serviceData={mockServiceData}
+        isUserAuthenticated={true}
+      />
+    );
+    fireEvent.click(screen.getByText(/Mock User/i));
+    expect(screen.getByText(/sign out/i)).toBeInTheDocument();
+  });
 });
