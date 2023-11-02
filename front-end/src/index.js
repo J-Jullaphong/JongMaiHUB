a266397917955688b71c713a0b449fdbe90c968a
Jullaphong JIAMWATTHANALOET <jullaphong03@gmail.com>
2023-11-02 08:45:51 +0700

    Update index files for firebase

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import "firebase/compat/auth";
+import firebase from "./firebase/index";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
