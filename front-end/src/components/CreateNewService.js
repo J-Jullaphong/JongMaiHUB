160b2b21f87c83fa6fa1bd26bdd49878c8bc736d
Pichayanon Toochinda <kewkunggt@gmail.com>
2023-11-04 16:00:44 +0700

    Update input information

--- front-end/src/components/CreateNewService.js
+++ front-end/src/components/CreateNewService.js
@@ -1,25 +1,59 @@
 import React, { useState } from 'react';
 import { Input, Button, Panel } from 'rsuite';
+import DataSender from './DataSender';
+import { useParams } from 'react-router-dom';
 
 const CreateNewService = () => {
+    const { providerId } = useParams();
     const [name, setName] = useState('');
+    const [type, setType] = useState('');
     const [duration, setDuration] = useState('');
     const [price, setPrice] = useState('');
+    const [servicePicture, setServicePicture] = useState("12345");
+    const [serviceProvider, setServiceProvider] = useState("");
 
-    const handleCreateService = () => {
-        const newService = {
+
+    const addServiceInfo = () => {
+        const dataSender = new DataSender();
+
+        const updatedServiceData = {
             name,
+            type,
             duration,
             price,
+            service_picture: servicePicture,
+            service_provider: providerId,
         };
-        setName('');
-        setDuration('');
-        setPrice('');
+
+        dataSender.submitServiceData(updatedServiceData).then(() => {
+            console.log('Service information updated.');
+        });
     };
 
     return (
         <Panel header="Create New Service">
-            <Button appearance="primary" onClick={handleCreateService}>
+            <h3>Service Information:</h3>
+            <Input
+                placeholder="Name"
+                value={name}
+                onChange={(value) => setName(value)}
+            />
+            <Input
+                placeholder="Type"
+                value={type}
+                onChange={(value) => setType(value)}
+            />
+            <Input
+                placeholder="Duration"
+                value={duration}
+                onChange={(value) => setDuration(value)}
+            />
+            <Input
+                placeholder="Price"
+                value={price}
+                onChange={(value) => setPrice(value)}
+            />
+            <Button appearance="primary" onClick={addServiceInfo}>
                 Create Service
             </Button>
         </Panel>
--- front-end/src/components/CreateNewStaff.js
+++ front-end/src/components/CreateNewStaff.js
@@ -1,28 +1,82 @@
 import React, { useState } from 'react';
 import { Input, Button, Panel } from 'rsuite';
+import DataSender from './DataSender';
+import { useParams } from 'react-router-dom';
+
+
 
 const CreateNewStaff = () => {
+    const { providerId } = useParams();
+    const [uid, setUid] = useState('');
     const [name, setName] = useState('');
-    const [duration, setDuration] = useState('');
-    const [price, setPrice] = useState('');
+    const [specialty, setSpecialty] = useState('');
+    const [background, setBackground] = useState('');
+    const [startWorkTime, setStartWorkTime] = useState('');
+    const [getOffWorkTime, setGetOffWorkTime] = useState('');
+    const [profilePicture, setProfilePicture] = useState("12345");
+    const [service_provider, setServiceProvider] = useState("");
+    const [service, setService] = useState(3);
+    const dataSender = new DataSender();
 
-    const handleCreateStaff = () => {
-        const newStaff = {
+
+    const addStaffInfo = () => {
+        console.log("provider", service_provider)
+        console.log("service", service)
+        const NewStaffData = {
+            uid,
             name,
-            duration,
-            price,
+            specialty,
+            background,
+            start_work_time: startWorkTime,
+            get_off_work_time: getOffWorkTime,
+            profile_picture: profilePicture,
+            service_provider: providerId,
+            service
         };
-        setName('');
-        setDuration('');
-        setPrice('');
-    };
 
+        dataSender.submitStaffData(NewStaffData).then(() => {
+            console.log('Staff information added.');
+        });
+    };
     return (
-        <Panel header="Create New Staff">
-            <Button appearance="primary" onClick={handleCreateStaff}>
-                Create Staff
-            </Button>
-        </Panel>
+        <div>
+            <Panel header={`Add new staff`}>
+                <h3>Staff Information</h3>
+                <Input
+                    placeholder="Uid"
+                    value={uid}
+                    onChange={(value) => setUid(value)}
+                />
+                <Input
+                    placeholder="Name"
+                    value={name}
+                    onChange={(value) => setName(value)}
+                />
+                <Input
+                    placeholder="Specialty"
+                    value={specialty}
+                    onChange={(value) => setSpecialty(value)}
+                />
+                <Input
+                    placeholder="Background"
+                    value={background}
+                    onChange={(value) => setBackground(value)}
+                />
+                <Input
+                    placeholder="Start Work Time"
+                    value={startWorkTime}
+                    onChange={(value) => setStartWorkTime(value)}
+                />
+                <Input
+                    placeholder="Get Off Work Time"
+                    value={getOffWorkTime}
+                    onChange={(value) => setGetOffWorkTime(value)}
+                />
+                <Button appearance="primary" onClick={addStaffInfo}>
+                    Add Staff Information
+                </Button>
+            </Panel>
+        </div>
     );
 };
 
