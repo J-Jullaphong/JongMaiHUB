58a07bdf18126584345c5c7611bac8dbbb2504f7
Pichayanon Toochinda <kewkunggt@gmail.com>
2023-11-04 17:54:58 +0700

    Add loading when load data

--- front-end/src/components/ProviderManagement.js
+++ front-end/src/components/ProviderManagement.js
@@ -15,16 +15,18 @@ const ProviderManagement = ({ user }) => {
     const [coverPicture, setCoverPicture] = useState('');
 
     const [staffList, setStaffList] = useState([]);
-    const [serviceList, setServiceList] = useState([])
+    const [serviceList, setServiceList] = useState([]);
 
     const [serviceData, setServiceData] = useState([]);
     const [providerData, setProviderData] = useState([]);
     const [staffData, setStaffData] = useState([]);
-    
+
+    const [loading, setLoading] = useState(true);
+
     const dataSender = new DataSender();
     const navigate = useNavigate();
     const dataFetcher = new DataFetcher();
-    
+
     useEffect(() => {
         if (currentProvider === null) {
             try {
@@ -32,13 +34,12 @@ const ProviderManagement = ({ user }) => {
                     const serviceData = await dataFetcher.getServiceByServiceProvider(user.getUID());
                     const providerData = await dataFetcher.getServiceProviderData(user.getUID());
                     const staffData = await dataFetcher.getStaffByServiceProvider(user.getUID());
-                    
+
                     setServiceData(serviceData);
                     setProviderData(providerData);
                     setStaffData(staffData);
 
                     if (user.isProvider) {
-
                         if (providerData) {
                             setCurrentProvider(providerData);
                             setUid(providerData.uid);
@@ -56,15 +57,16 @@ const ProviderManagement = ({ user }) => {
                             setServiceList(serviceData);
                         }
                     }
+
+                    setLoading(false); 
                 };
                 fetchData();
             } catch (error) {
                 console.error(error);
+                setLoading(false); 
             }
-        }        
+        }
     }, [user.isProvider, user.id, providerData, staffData, serviceData]);
-
-    
 
     const updateProviderInfo = () => {
         const providerData = {
@@ -76,7 +78,7 @@ const ProviderManagement = ({ user }) => {
             profile_picture: profilePicture,
             coverPicture,
         };
-        
+
         dataSender.updateServiceProviderData(providerData, currentProvider.uid).then(() => {
             console.log('Provider information updated.');
         });
@@ -93,6 +95,7 @@ const ProviderManagement = ({ user }) => {
             }
         }
     };
+
     const addNewStaff = (providerId) => {
         navigate(`/add-staff/${providerId}`);
     };
@@ -105,7 +108,7 @@ const ProviderManagement = ({ user }) => {
         navigate(`/staff-management/${staffUid}`);
     };
 
-    const handleServiceSelection = ( serviceId) => {
+    const handleServiceSelection = (serviceId) => {
         navigate(`/service-management/${serviceId}`);
     };
 
@@ -118,160 +121,165 @@ const ProviderManagement = ({ user }) => {
         dataSender.deleteService(serviceId);
         navigate(`/provider-management`);
     };
- 
+
     return (
-        <div>
-            <Panel header="Provider Management">
-                <h3>Current Provider Information</h3>
-                <img src={profilePicture} alt="Profile Picture" />
-                <div>
-                    <label>Profile Picture</label>
-                    <input
-                        type="file"
-                        accept="image/*"
-                        onChange={uploadImage}
-                    />
-                </div>
-                <Input
-                    placeholder="Name"
-                    value={name}
-                    onChange={(value) => setName(value)}
-                />
-                <Input
-                    placeholder="Location"
-                    value={location}
-                    onChange={(value) => setLocation(value)}
-                />
-                <Input
-                    placeholder="Opening Time"
-                    value={openingTime}
-                    onChange={(value) => setOpeningTime(value)}
-                />
-                <Input
-                    placeholder="Closing Time"
-                    value={closingTime}
-                    onChange={(value) => setClosingTime(value)}
-                />
-                <Button appearance="primary" onClick={updateProviderInfo}>
-                    Update Information
-                </Button>
-            </Panel>
+        <div className="ProviderManagement">
+            {loading ? (
+                <div>Loading...</div>
+            ) : (
+                <>
+                    <Panel header="Provider Management">
+                        <h3>Current Provider Information</h3>
+                        <img src={profilePicture} alt="Profile Picture" />
+                        <div>
+                            <label>Profile Picture</label>
+                            <input
+                                type="file"
+                                accept="image/*"
+                                onChange={uploadImage}
+                            />
+                        </div>
+                        <Input
+                            placeholder="Name"
+                            value={name}
+                            onChange={(value) => setName(value)}
+                        />
+                        <Input
+                            placeholder="Location"
+                            value={location}
+                            onChange={(value) => setLocation(value)}
+                        />
+                        <Input
+                            placeholder="Opening Time"
+                            value={openingTime}
+                            onChange={(value) => setOpeningTime(value)}
+                        />
+                        <Input
+                            placeholder="Closing Time"
+                            value={closingTime}
+                            onChange={(value) => setClosingTime(value)}
+                        />
+                        <Button appearance="primary" onClick={updateProviderInfo}>
+                            Update Information
+                        </Button>
+                    </Panel>
 
-            <Panel header="Staff Management">
-                <Table
-                    data={staffList}
-                    autoHeight
-                    width={1000}
-                >
-                    <Table.Column width={200}>
-                        <Table.HeaderCell>Staff Name</Table.HeaderCell>
-                        <Table.Cell>
-                            {rowData => (
-                                <p>
-                                    {rowData.name} 
-                                </p>
-                            )}
-                        </Table.Cell>
-                    </Table.Column>
-                    <Table.Column width={200}>
-                        <Table.HeaderCell>Specialty</Table.HeaderCell>
-                        <Table.Cell dataKey="specialty" />
-                    </Table.Column>
-                    <Table.Column width={200}>
-                        <Table.HeaderCell>Specialty</Table.HeaderCell>
-                        <Table.Cell dataKey="service" />
-                    </Table.Column>
-                    <Table.Column width={200}>
-                        <Table.HeaderCell>Update</Table.HeaderCell>
-                        <Table.Cell>
-                            {rowData => (
-                                <button onClick={() => handleStaffSelection(rowData.uid)}>
-                                    Update
-                                </button>
-                            )}
-                        </Table.Cell>
-                    </Table.Column>
-                    <Table.Column width={200}>
-                        <Table.HeaderCell>Delete</Table.HeaderCell>
-                        <Table.Cell>
-                            {rowData => (
-                                <button onClick={() => deleteStaffSelection(rowData.uid)}>
-                                    Delete
-                                </button>
-                            )}
-                        </Table.Cell>
-                    </Table.Column>
-                </Table>
-                <Button appearance="primary"  onClick={() => addNewStaff(currentProvider.uid)}>
-                    Add Staff
-                </Button>
-            </Panel>
+                    <Panel header="Staff Management">
+                        <Table
+                            data={staffList}
+                            autoHeight
+                            width={1000}
+                        >
+                            <Table.Column width={200}>
+                                <Table.HeaderCell>Staff Name</Table.HeaderCell>
+                                <Table.Cell>
+                                    {rowData => (
+                                        <p>
+                                            {rowData.name}
+                                        </p>
+                                    )}
+                                </Table.Cell>
+                            </Table.Column>
+                            <Table.Column width={200}>
+                                <Table.HeaderCell>Specialty</Table.HeaderCell>
+                                <Table.Cell dataKey="specialty" />
+                            </Table.Column>
+                            <Table.Column width={200}>
+                                <Table.HeaderCell>Specialty</Table.HeaderCell>
+                                <Table.Cell dataKey="service" />
+                            </Table.Column>
+                            <Table.Column width={200}>
+                                <Table.HeaderCell>Update</Table.HeaderCell>
+                                <Table.Cell>
+                                    {rowData => (
+                                        <button onClick={() => handleStaffSelection(rowData.uid)}>
+                                            Update
+                                        </button>
+                                    )}
+                                </Table.Cell>
+                            </Table.Column>
+                            <Table.Column width={200}>
+                                <Table.HeaderCell>Delete</Table.HeaderCell>
+                                <Table.Cell>
+                                    {rowData => (
+                                        <button onClick={() => deleteStaffSelection(rowData.uid)}>
+                                            Delete
+                                        </button>
+                                    )}
+                                </Table.Cell>
+                            </Table.Column>
+                        </Table>
+                        <Button appearance="primary" onClick={() => addNewStaff(currentProvider.uid)}>
+                            Add Staff
+                        </Button>
+                    </Panel>
 
-                <Panel header="Service of Venues">
-                    <Table
-                        data={serviceList}
-                        autoHeight
-                        width={1000}
-                    >
-                        <Table.Column width={200}>
-                            <Table.HeaderCell>Service Name</Table.HeaderCell>
-                            <Table.Cell>
-                                {rowData => (
-                                    <p>
-                                        {rowData.name}
-                                    </p>
-                                )}
-                            </Table.Cell>
-                        </Table.Column>
-                        <Table.Column width={200}>
-                        <Table.HeaderCell>Service duration</Table.HeaderCell>
-                        <Table.Cell>
-                            {rowData => (
-                                <p>
-                                    {rowData.duration}
-                                </p>
-                            )}
-                        </Table.Cell>
-                        </Table.Column>
-                        <Table.Column width={200}>
-                        <Table.HeaderCell>Service price</Table.HeaderCell>
-                        <Table.Cell>
-                            {rowData => (
-                                <p>
-                                    {rowData.price}
-                                </p>
-                            )}
-                        </Table.Cell>
-                    </Table.Column>
-                    <Table.Column width={200}>
-                        <Table.HeaderCell>Update</Table.HeaderCell>
-                        <Table.Cell>
-                            {rowData => (
-                                <button onClick={() => handleServiceSelection(rowData.id)}>
-                                    Update
-                                </button>
-                            )}
-                        </Table.Cell>
-                    </Table.Column>
-                    <Table.Column width={200}>
-                        <Table.HeaderCell>Delete</Table.HeaderCell>
-                        <Table.Cell>
-                            {rowData => (
-                                <button onClick={() => deleteServiceSelection(rowData.id)}>
-                                    Delete
-                                </button>
-                            )}
-                        </Table.Cell>
-                    </Table.Column>
-                    </Table>
-                    
-                <Button appearance="primary"  onClick={() => addNewService(currentProvider.uid)}>
-                    Add Service
-                </Button>
-            </Panel>
+                    <Panel header="Service of Venues">
+                        <Table
+                            data={serviceList}
+                            autoHeight
+                            width={1000}
+                        >
+                            <Table.Column width={200}>
+                                <Table.HeaderCell>Service Name</Table.HeaderCell>
+                                <Table.Cell>
+                                    {rowData => (
+                                        <p>
+                                            {rowData.name}
+                                        </p>
+                                    )}
+                                </Table.Cell>
+                            </Table.Column>
+                            <Table.Column width={200}>
+                                <Table.HeaderCell>Service duration</Table.HeaderCell>
+                                <Table.Cell>
+                                    {rowData => (
+                                        <p>
+                                            {rowData.duration}
+                                        </p>
+                                    )}
+                                </Table.Cell>
+                            </Table.Column>
+                            <Table.Column width={200}>
+                                <Table.HeaderCell>Service price</Table.HeaderCell>
+                                <Table.Cell>
+                                    {rowData => (
+                                        <p>
+                                            {rowData.price}
+                                        </p>
+                                    )}
+                                </Table.Cell>
+                            </Table.Column>
+                            <Table.Column width={200}>
+                                <Table.HeaderCell>Update</Table.HeaderCell>
+                                <Table.Cell>
+                                    {rowData => (
+                                        <button onClick={() => handleServiceSelection(rowData.id)}>
+                                            Update
+                                        </button>
+                                    )}
+                                </Table.Cell>
+                            </Table.Column>
+                            <Table.Column width={200}>
+                                <Table.HeaderCell>Delete</Table.HeaderCell>
+                                <Table.Cell>
+                                    {rowData => (
+                                        <button onClick={() => deleteServiceSelection(rowData.id)}>
+                                            Delete
+                                        </button>
+                                    )}
+                                </Table.Cell>
+                            </Table.Column>
+                        </Table>
+
+                        <Button appearance="primary" onClick={() => addNewService(currentProvider.uid)}>
+                            Add Service
+                        </Button>
+                    </Panel>
+                </>
+            )}
         </div>
     );
 };
 
 export default ProviderManagement;
-
