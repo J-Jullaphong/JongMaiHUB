35d7123639161d736c991ea080249ae2f8a4a172
MeHappyLucky <menggy2546@gmail.com>
2023-11-09 23:25:01 +0700

    minor changes, only to commit and push upstream to be pull in another device

--- front-end/src/components/HubScreen.js
+++ front-end/src/components/HubScreen.js
@@ -1,80 +1,82 @@
 import React, { Component } from "react";
 import { Carousel, Panel, List, Row, Col, Button } from "rsuite";
 import "./styles/HubScreen.css";
+import DataFetcher from './DataFetcher';
 
 const data = {
     name: "Service Name",
     type: "Service Type",
     rating: "Service Rating",
-  };
+};
 
 const handleCardClick = () => {
     alert("Clicked!");
 };
 
-const Card = () => (
-    <Panel 
-        shaded 
-        bordered 
-        style={{ 
-            display: 'inline-block', 
-            width: 360,
-            cursor:'pointer',
-            transition: 'background-color 0.2s',
-            backgroundColor: 'white',
-            }}
-        onClick={handleCardClick}
-    >
-        <table className="card" style={{ width: '100%' }}>
-            <tr>
-                <td style={{ padding: '10px'}}>
-                    <img src="https://via.placeholder.com/100x100" height="100" width="100" alt="Image" />
-                </td>
-                <td style={{ verticalAlign: 'top', padding: '10px', textAlign: 'left'}}>
-                    <p>{data.name}</p>
-                    <p>{data.type}</p>
-                    <p>{data.rating}</p>
-                </td>
-            </tr>
-        </table>
-    </Panel>
-);
+const Card = () => {
+    const service = new DataFetcher().getServiceData();
+    return (
+        <Panel 
+            shaded 
+            bordered 
+            style={{ 
+                display: 'inline-block', 
+                width: 360,
+                cursor:'pointer',
+                transition: 'background-color 0.2s',
+                backgroundColor: 'white',
+                }}
+            onClick={handleCardClick}
+        >
+            <table className="card" style={{ width: '100%' }}>
+                <tr>
+                    <td style={{ padding: '10px'}}>
+                        <img src="https://via.placeholder.com/100x100" height="100" width="100" alt="Image" />
+                    </td>
+                    <td style={{ verticalAlign: 'top', padding: '10px', textAlign: 'left'}}>
+                        <p>{service.name}</p>
+                        <p>{service.type}</p>
+                        <p>Rating</p>
+                    </td>
+                </tr>
+            </table>
+        </Panel>
+    )
+}
 
-class HubScreen extends Component {
-    render() {
-        return (
-            <div className="container">
-                <div className="group">
-                    <h2>Popular Services</h2>
-                </div>
-                <div className="popular-carousel">
-                    <Carousel autoplay style={{ borderRadius: 50 }}>
-                        <img src="images/beauty-clinic.png" />
-                        <img src="images/barber.png" />
-                    </Carousel>
-                </div>
-                <div className="group">
-                    <h2>Recommended Services</h2>
-                </div>
-                <div className="recommend-panel">
-                <Row>
-                    <Col md={6} sm={12}>
-                        <Card />
-                    </Col>
-                    <Col md={6} sm={12}>
-                        <Card />
-                    </Col>
-                    <Col md={6} sm={12}>
-                        <Card />
-                    </Col>
-                    <Col md={6} sm={12}>
-                      <Card />
-                    </Col>
-                </Row>
-                </div>
+const HubScreen = () => {
+    return (
+        <div className="container">
+            <div className="group">
+                <h2>Popular Services</h2>
             </div>
-        );
-    }
+            <div className="popular-carousel">
+                <Carousel autoplay style={{ borderRadius: 50 }}>
+                    <img src="images/beauty-clinic.png" />
+                    <img src="images/barber.png" />
+                </Carousel>
+            </div>
+            <div className="group">
+                <h2>Recommended Services</h2>
+            </div>
+            <div className="recommend-panel">
+            <Row>
+                <Col md={6} sm={12}>
+                    <Card />
+                </Col>
+                <Col md={6} sm={12}>
+                    <Card />
+                </Col>
+                <Col md={6} sm={12}>
+                    <Card />
+                </Col>
+                <Col md={6} sm={12}>
+                  <Card />
+                </Col>
+            </Row>
+            </div>
+        </div>
+    );
 }
 
 export default HubScreen;
