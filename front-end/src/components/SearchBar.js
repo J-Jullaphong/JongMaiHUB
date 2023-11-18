e3d407c4059bf6a57650aaacfd22c8cf837dcc3c
Jullaphong JIAMWATTHANALOET <jullaphong03@gmail.com>
2023-11-18 20:16:37 +0700

    Reformat code in SeachScreen, SearchBar, and Filter components

--- front-end/src/components/SearchBar.js
+++ front-end/src/components/SearchBar.js
@@ -4,41 +4,38 @@ import SearchIcon from "@rsuite/icons/Search";
 import { useNavigate } from "react-router-dom";
 
 const SearchBar = () => {
-    const [searchQuery, setSearchQuery] = useState("");
-    const navigate = useNavigate();
+  const [searchQuery, setSearchQuery] = useState("");
+  const navigate = useNavigate();
 
-    const handleSearch = () => {
-        if (searchQuery !== "") {
-            navigate("/search?name=" + searchQuery);
-        }
-    };
+  const handleSearch = () => {
+    if (searchQuery !== "") {
+      navigate("/search?name=" + searchQuery);
+    }
+  };
 
-    const handleSearchQueryChange = (value) => {
-        setSearchQuery(value);
-    };
-    
-    const handleKeyPress = (event) => {
-        if (event.key === "Enter") {
-            event.preventDefault();
-            if (searchQuery.trim() === "") {
-                navigate("/search");
-            } else {
-                handleSearch();
-            }
-        }
-    };
+  const handleSearchQueryChange = (value) => {
+    setSearchQuery(value);
+  };
 
-    return (
-        <InputGroup inside style={{ width: "30vw" }} onKeyDown={handleKeyPress}>
-            <AutoComplete
-                onChange={handleSearchQueryChange}
-                value={searchQuery}
-            />
-            <InputGroup.Button onClick={handleSearch}>
-                <SearchIcon />
-            </InputGroup.Button>
-        </InputGroup>
-    );
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      if (searchQuery.trim() === "") {
+        navigate("/search");
+      } else {
+        handleSearch();
+      }
+    }
+  };
+
+  return (
+    <InputGroup inside style={{ width: "30vw" }} onKeyDown={handleKeyPress}>
+      <AutoComplete onChange={handleSearchQueryChange} value={searchQuery} />
+      <InputGroup.Button onClick={handleSearch}>
+        <SearchIcon />
+      </InputGroup.Button>
+    </InputGroup>
+  );
 };
 
 export default SearchBar;
